e04d1d7f1e8c91094182a283ce266538bbecfe17
Anka <runner@sat12-dp155-9729bc3a-34cf-495b-9bb0-c0dfd998c6cf-FAEA24980866.local>
2026-05-27 23:42:04 +0000

    port-audit 0.1.13

--- Formula/port-audit.rb
+++ Formula/port-audit.rb
@@ -1,8 +1,8 @@
 class PortAudit < Formula
   desc "TUI tool for managing TCP listening ports on macOS"
   homepage "https://github.com/hehehai/port-audit"
-  url "https://github.com/hehehai/port-audit/releases/download/v0.1.12/port-audit-v0.1.12-macos.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/hehehai/port-audit/releases/download/v0.1.13/port-audit-v0.1.13-macos.tar.gz"
+  sha256 "ecedfa501bfb15debcadf4db4e2804f184a8a58f2da526904accc1ff02f36ea4"
   license "MIT"
 
   def install
